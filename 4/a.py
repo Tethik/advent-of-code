dcfea2fc19bbfc538b4c2ec03b7fdeae8b8fe78f
Joakim Uddholm <tethik@blacknode.se>
2021-12-04 15:14:24 +0100

    day 4 solved

--- 4/a.py
+++ 4/a.py
@@ -2,24 +2,14 @@ import sys
 from itertools import chain
 
 
-def tokenize(line: str):
-    tok = ""
-    for c in line:
-        if c.strip() == "":
-            if len(tok) > 0:
-                yield Cell(tok)
-                tok = ""
-            continue
-        tok += c
-
-
 class Cell:
     def __init__(self, value) -> None:
         self.value = int(value)
         self.marked = False
+        self.subscribed = []
 
-    def mark(self, number):
-        self.marked = self.marked or number == self.value
+    def mark(self):
+        self.marked = True
 
     def __str__(self) -> str:
         if self.marked:
@@ -30,17 +20,35 @@ class Cell:
         return str(self)
 
 
+cellRefs = dict([(val, Cell(val)) for val in range(100)])
+
+
+def tokenize(line: str):
+    tok = ""
+    for c in line:
+        if c.strip() == "":
+            if len(tok) > 0:
+                yield cellRefs.get(int(tok))
+                tok = ""
+        else:
+            tok += c
+
+
 CARD_WIDTH = 5
 
 
 class Card:
-    def __init__(self, lines) -> None:
+    def __init__(self, id, lines) -> None:
+        self.id = id
         self.rows = []
         self.cols = []
-        self.left_diagonal = []
-        self.right_diagonal = []
+        # Oops, did not need diagonal :)
+        # self.left_diagonal = []
+        # self.right_diagonal = []
 
         self.rows = [list(tokenize(line)) for line in lines]
+        for cell in chain.from_iterable(self.rows):
+            cell.subscribed.append(self)
 
         for x in range(CARD_WIDTH):
             self.cols.append([])
@@ -52,18 +60,14 @@ class Card:
         print(self.cols)
         print()
 
-        for d in range(CARD_WIDTH):
-            self.left_diagonal.append(self.rows[d][d])
-            self.right_diagonal.append(
-                self.rows[CARD_WIDTH - 1 - d][CARD_WIDTH - 1 - d])
-
-    def mark(self, number) -> bool:
-        for cell in chain.from_iterable(self.rows):
-            cell.mark(number)
-        return self.check()
+        # for d in range(CARD_WIDTH):
+        #     self.left_diagonal.append(self.rows[d][d])
+        #     self.right_diagonal.append(
+        #         self.rows[CARD_WIDTH - 1 - d][CARD_WIDTH - 1 - d])
 
     def check(self):
-        for l in chain.from_iterable([self.rows, self.cols, [self.right_diagonal, self.left_diagonal]]):
+        # , [self.right_diagonal, self.left_diagonal]]):
+        for l in chain.from_iterable([self.rows, self.cols]):
             # print(l)
             if all(c.marked for c in l):
                 print(l)
@@ -78,7 +82,7 @@ class Card:
         s = ""
         for row in self.rows:
             for cell in row:
-                s += f"{str(cell):<6} "
+                s += f"{str(cell):<5}"
             s += "\n"
         return s
 
@@ -87,38 +91,52 @@ call_outs = input().split(",")
 
 lines = []
 cards = []
-for line in sys.stdin.readlines():
+i = 1
+for line in sys.stdin:
     if line.strip() == "":
         continue
 
     lines.append(line)
 
     if len(lines) == 5:
-        card = Card(lines)
+        card = Card(i, lines)
         cards.append(card)
         lines = []
+        i += 1
         continue
+
+print("Boards:")
+for card in cards:
+    print(card)
+    print()
+print()
 
 
 won = dict()
+print("Simlating callouts:")
 for t, call in enumerate([int(c) for c in call_outs]):
     print(f"Callout: {call} (t: {t})")
-    for i, card in enumerate(cards):
-        if i in won.keys():
+    cell = cellRefs[call]
+    cell.marked = True
+
+    for card in cell.subscribed:
+        if card.id in won.keys():
             continue
 
-        # print(f"Checking card {i}")
-        if card.mark(call):
-            print(f"Bingo on card {i}!")
+        if card.check():
+            print(f"Bingo on card {card.id}!")
             print(card)
-            # print(call, card.sum(), call * card.sum())
-            won[i] = (t, call, card.sum(), call * card.sum())
+            print(f"Sum: {call} * {card.sum()} = {call * card.sum()}")
+            print(f"Callouts: {call_outs[:t+1]}")
+            won[card.id] = (t, call, card.sum(), call * card.sum())
             print()
 
     print()
     if len(won) == len(cards):
         break
 
+print()
+print("Winning Table:")
 for key, val in sorted(won.items(), key=lambda dict_item: dict_item[1][0]):
     print(key, val)
 
