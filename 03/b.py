79c2d968db01790e319a943cca8337d08b51246a
Joakim Uddholm <tethik@blacknode.se>
2022-12-03 06:45:32 +0100

    some cleanup

--- 03/b.py
+++ 03/b.py
@@ -11,14 +11,16 @@ for line in sys.stdin:
 
     seen = set()
     for c in group[0]:
-        if c in group[1] and c in group[2]:
-            if c not in seen:
-                if c < 'a':
-                    p = ord(c) - ord('A') + 27
-                else:
-                    p = ord(c) - ord('a') + 1
-                print(c, p)
-                s += p
+        if c in seen:
+            continue
+
+        if all([c in g for g in group]):
+            if c < 'a':
+                p = ord(c) - ord('A') + 27
+            else:
+                p = ord(c) - ord('a') + 1
+            # print(c, p)
+            s += p
         seen.add(c)
 
     group = []
