c6d3881a4320276127663663e1104c799cdc71eb
Joakim Uddholm <tethik@blacknode.se>
2022-12-03 07:13:51 +0100

    loop through set instead

--- 03/b.py
+++ 03/b.py
@@ -9,19 +9,14 @@ for line in sys.stdin:
     if len(group) != 3:
         continue
 
-    seen = set()
-    for c in group[0]:
-        if c in seen:
-            continue
-
-        if all([c in g for g in group]):
+    for c in set(group[0]):
+        if all(c in g for g in group):
             if c < 'a':
                 p = ord(c) - ord('A') + 27
             else:
                 p = ord(c) - ord('a') + 1
             # print(c, p)
             s += p
-        seen.add(c)
 
     group = []
 
