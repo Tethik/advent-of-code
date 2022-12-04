8e9c1cf43acb55a0e5d23e15e58ad9af41da4247
Joakim Uddholm <tethik@blacknode.se>
2022-12-04 07:06:04 +0100

    better overlap function. thanks gpt

--- 04/a.py
+++ 04/a.py
@@ -2,11 +2,13 @@ import sys
 
 
 def contains(p1, p2):
-    r = int(p1[1]) >= int(p2[1]) and int(p1[0]) <= int(p2[0])
-    if r:
-        print(p1, p2)
-        print(r)
-    return r
+    # Unpack the tuples to get the start and end of each range
+    a, b = p1
+    c, d = p2
+
+    # Check if the ranges overlap by checking if the end of the first range
+    # is greater than the start of the second range, and vice versa
+    return (a <= d) and (b >= c)
 
 
 s = 0
