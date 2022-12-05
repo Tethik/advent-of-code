1575846821664d07c95cf07fb3d61349d6795fbe
Joakim Uddholm <tethik@gmail.com>
2022-12-05 10:25:49 +0100

    remove unused code

--- 05/b.py
+++ 05/b.py
@@ -1,17 +1,6 @@
 import re
 import sys
 
-
-def contains(p1, p2):
-    # Unpack the tuples to get the start and end of each range
-    a, b = p1
-    c, d = p2
-
-    # Check if the ranges overlap by checking if the end of the first range
-    # is greater than the start of the second range, and vice versa
-    return (a <= d) and (b >= c)
-
-
 INPUT_MODE = "stacks"
 stacks = []
 inst = []
