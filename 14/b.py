b1fe07d8984aa34acd123502683bbba049b0a141
Joakim Uddholm <tethik@blacknode.se>
2022-12-14 22:42:51 +0100

    remove some unneeded code

--- 14/b.py
+++ 14/b.py
@@ -56,7 +56,6 @@ for y in range(1, len(_map) - 1):
         if _map[y][x] == '#':
             continue
         parents = [_map[y - 1][x]]
-        # [, , _map[r - 1][x + 1]]:
         if x > 0:
             parents += _map[y - 1][x - 1]
         if x < len(_map[y]) - 1:
